6689e8c7c38e38dc89fc66e6882845b096177054
dovcharov <djuliqn.bg@gmail.com>
2020-02-15 12:34:20 +0200

    Update dependencies

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { MaterialModule } from './material.module';
 import { AuthService } from '../services/auth.service';
 import { JwtHelperService } from '../helpers/JwtHelperService';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,16 +12,16 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
-    MaterialModule,
-    RouterModule
+    MaterialModule
   ],
   providers: [
     AuthService,
     JwtHelperService
-  ] 
+  ]
 })
 export class SharedModule { }
