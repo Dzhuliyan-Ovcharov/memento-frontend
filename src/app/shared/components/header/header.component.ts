abf8361eef8310cbf4b254f4ea6a7bebaa8c6182
dovcharov <djuliqn.bg@gmail.com>
2020-02-17 21:03:43 +0200

    remove unused imports

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { JwtHelperService } from 'src/app/core/services/jwt-helper.service';
 
 @Component({
